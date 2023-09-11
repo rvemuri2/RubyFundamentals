1c4a7b29471ef9d433e82a54ec1c0d35500d5234
Rohit Vemuri <rvemuri2@ucmerced.edu>
2023-09-10 23:33:44 -0700

    finished basics of hashmaps

--- hashmaps.rb
+++ hashmaps.rb
@@ -21,3 +21,55 @@ me = {name: "Rohit", age: 23, hobbies: ["cricket", "reading", "coding"]}
 puts me
 
 puts "#{me[:hobbies]}"
+puts "#{me[:hobbies].join(", ")}"
+puts me[:hobbies].size() 
+
+pets = {
+    dog: {name: "Sport"}, 
+    cat: {name: "Yarn"}
+}
+puts pets
+puts pets[:dog].values()
+puts pets[:dog][:name] #Another way of getting specific value
+puts pets.dig(:dog, :name) #Another way of getting specific value
+
+me[:hobbies].each do |hobby| 
+    puts hobby.capitalize 
+end 
+
+me[:hobbies].each do |hobby| 
+    print hobby.capitalize 
+end 
+me[:hobbies].each do |hobby| 
+    p hobby.capitalize 
+end 
+
+me.each do |details|
+    puts details
+end
+
+me.each do |details|
+    p details
+end
+
+me.each do |key, val|
+    puts "#{key} => #{val}"
+end
+
+puts me[:location]
+
+new_age = {age: 24}
+
+puts me.merge(new_age)
+
+puts me
+
+h = {["foo", "bar"] => "baz"} 
+
+puts h[["foo", "bar"]] 
+
+a = {name: "Cooper"}
+b = {"name" => "Chris"}
+
+puts a[:name]
+puts b["name"]
